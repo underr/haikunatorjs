de25f62edc62d2ab96ad42c8a814ac49635f0685
underr <under@insicuri.net>
2015-04-21 12:15:56 -0300

    Tradução para PT-BR com feminino e masculino

--- index.js
+++ index.js
@@ -1,31 +1,49 @@
-var adjs = [
-  "autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark",
-  "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter",
-  "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue",
-  "billowing", "broken", "cold", "damp", "falling", "frosty", "green",
-  "long", "late", "lingering", "bold", "little", "morning", "muddy", "old",
-  "red", "rough", "still", "small", "sparkling", "throbbing", "shy",
-  "wandering", "withered", "wild", "black", "young", "holy", "solitary",
-  "fragrant", "aged", "snowy", "proud", "floral", "restless", "divine",
-  "polished", "ancient", "purple", "lively", "nameless", "lucky", "odd", "tiny",
-  "free", "dry", "yellow", "orange", "gentle", "tight", "super", "royal", "broad",
-  "steep", "flat", "square", "round", "mute", "noisy", "hushy", "raspy", "soft",
-  "shrill", "rapid", "sweet", "curly", "calm", "jolly", "fancy", "plain", "shinny"
+var adjsF = [
+  "escondida", "amarga", "nebulosa", "silenciosa", "vazia", "seca", "escura",
+  "gelada", "delicada", "quieta", "branca", "legal", "paciente", "carmesin",
+  "fina", "encharcada", "azul", "esvoaçante", "quebrada", "fria", "úmida",
+  "caindo", "verde", "longa", "tardia", "prolongada", "corajosa", "pequena",
+  "lamacenta", "velha", "vermelha", "brusca", "imóvel", "pequena", "cintilante",
+  "latejante", "tímida", "errante", "murcha", "selvagem", "preta", "jovem",
+  "sagrada", "solitária", "perfumada", "idosa", "pura", "orgulhosa", "floral",
+  "inquieta", "divina", "elegante", "antiquada", "roxa", "animada", "desconhecida",
+  "sortuda", "real", "ampla", "íngreme", "plana", "redonda", "muda", "barulhenta",
+  "rude", "rouca", "suave", "estridente", "rápida", "doce", "encaracolada", "calma",
+  "alegre", "estravagante", "plana", "brilhante"
 ];
-var nouns = [
-  "waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning",
-  "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter",
-  "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook",
-  "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly",
-  "feather", "grass", "haze", "mountain", "night", "pond", "darkness",
-  "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder",
-  "violet", "water", "wildflower", "wave", "water", "resonance", "sun",
-  "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper",
-  "frog", "smoke", "star", "atom", "band", "bar", "base", "block", "boat",
-  "term", "credit", "art", "fashion", "truth", "disk", "math", "unit", "cell",
-  "scene", "heart", "recipe", "union", "limit", "bread", "toast", "bonus",
-  "lab", "mud", "mode", "poetry", "tooth", "hall", "king", "queen", "lion", "tiger",
-  "penguin", "kiwi", "cake", "mouse", "rice", "coke", "hola", "salad", "hat"
+
+var adjsM = [
+  "escondido", "amargo", "nebuloso", "silencioso", "vazio", "seco", "escuro",
+  "gelado", "delicado", "quieto", "branco", "legal", "paciente", "carmesin",
+  "fino", "encharcado", "azul", "esvoaçante", "quebrado", "frio", "úmido",
+  "caindo", "verde", "longo", "tardio", "prolongado", "corajoso", "pequeno",
+  "lamacento", "velho", "vermelho", "brusco", "imóvel", "pequeno", "cintilante",
+  "latejante", "tímido", "errante", "murcho", "selvagem", "preto", "jovem",
+  "sagrado", "solitário", "perfumado", "idoso", "puro", "orgulhoso", "floral",
+  "inquieto", "divino", "elegante", "antiquado", "roxo", "animado", "desconhecido",
+  "sortudo", "real", "amplo", "íngreme", "plano", "redondo", "mudo", "barulhento",
+  "rude", "rouco", "suave", "estridente", "rápido", "doce", "encaracolado", "calmo",
+  "alegre", "estravagante", "plano", "brilhante"
+];
+
+var subsM = [
+  "frio", "vento", "oceano", "amanhecer", "brilho", "lago", "pinho", "pasto", 
+  "sol", "pássaro", "riacho", "arbusto", "orvalho", "campo", "fogo", "silêncio",
+  "som", "céu", "trovão", "sol", "sonho", "árvore", "papel", "sapo", "átomo",
+  "bloco", "barco", "prazo", "crédito", "disco", "coração", "limite", "pão",
+  "bônus", "laboratório", "modo", "dente", "rei", "leão", "tigre", "pinguim",
+  "kiwi", "bolo", "rato", "arroz", "chapéu"
+];
+
+var subsF= [
+  "cachoeira",  "brisa", "lua", "chuva", "manhã", "neve", "sombra", "folha", 
+  "floresta", "colina", "nuvem", "clareira", "borboleta", "poeira", "flor",
+  "pena", "grama", "neblina", "montanha", "noite", "lagoa", "escuridão",
+  "forma", "ressaca", "violeta", "água", "onda", "maré", "ressonância",
+  "madeira", "cereja", "névoa", "geada", "voz", "fumaça", "estrela",
+  "banda", "barra", "base", "arte", "moda", "verdade", "matemática",
+  "unidade", "célula", "cena", "receita", "união", "torrada", "lama",
+  "poesia", "sala", "rainha", "coca", "salada"
 ];
 
 function extend(obj) {
@@ -41,7 +59,14 @@ function extend(obj) {
 }
 
 var haikunator = function (opt) {
-  var i, adj, noun, sections, defaults, token = "";
+  if ((Math.floor((Math.random() * 100) + 1)) % 2 == 0) {
+    adjs = adjsF;
+    subs = subsF;
+  } else {
+    adjs = adjsM;
+    subs = subsM;
+  }
+  var i, adj, sub, sections, defaults, token = "";
 
   defaults = {
     delimiter: "-",
@@ -57,13 +82,13 @@ var haikunator = function (opt) {
   }
 
   adj = adjs[Math.floor(Math.random() * adjs.length)];
-  noun = nouns[Math.floor(Math.random() * nouns.length)];
+  sub = subs[Math.floor(Math.random() * subs.length)];
 
   for (i = 0; i < opt.tokenLength; i++) {
     token += opt.tokenChars.charAt(Math.floor(Math.random() * opt.tokenChars.length));
   }
 
-  sections = [adj, noun, token];
+  sections = [sub, adj, token];
   return sections.filter(function (e) { return e === 0 || e; }).join(opt.delimiter);
 };
 
